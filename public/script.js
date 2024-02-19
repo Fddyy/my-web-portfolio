45bdffd6222b7525fb1bcfc8f91114d9153c08c5
Ferdy <frdytmvn@gmail.com>
2024-02-19 22:17:24 +0700

    menambahkan navbar

--- public/script.js
+++ public/script.js
@@ -1,6 +1,9 @@
+
 // Hamburger
 const hb = document.querySelector('#hamburger');
+const navmenu = document.querySelector('#nav-menu');
 
 hb.addEventListener('click', function() {
     hb.classList.toggle('hamburger-aktif');
+    navmenu.classList.toggle('hidden')
 });

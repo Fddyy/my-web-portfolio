5a0a0a29d4c80b06fdbf979e9eeb2831f4a85e11
Ferdy <frdytmvn@gmail.com>
2024-02-21 23:31:23 +0700

    menambahkan icon pada tittle dan menambahkan alert saat gagal mengirim data pada form contact

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,3 @@
-
 // Hamburger
 const hb = document.querySelector('#hamburger');
 const navmenu = document.querySelector('#nav-menu');
@@ -7,3 +6,18 @@ hb.addEventListener('click', function() {
     hb.classList.toggle('hamburger-aktif');
     navmenu.classList.toggle('hidden')
 });
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxGoPifMcR1cYFL4Xkssvizj6Mt59UUMCayrEvOu4QrSTZuZtgRbzfGr9eVITD63vSJ/exec'
+    const form = document.forms['My-web-portfolio contact form'];
+    const myAlert = document.getElementById('alert');
+
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+          myAlert.classList.toggle('hidden')
+          console.log('succsess!', response)
+          form.reset();
+        })
+        .catch(error => alert(error.message))
+    })
